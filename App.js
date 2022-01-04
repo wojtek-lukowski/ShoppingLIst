5d4bce99e954177ffccb424c4a90eda73c28504b
Wojtek Lukowski <wojtek_lukowski@yahoo.com>
2022-01-04 18:18:24 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,13 +1,74 @@
+import React from 'react';
+import { FlatList } from 'react-native-web';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const firebase = require('firebase');
+require('firebase/firestore');
+
+export default class App extends React.Component {
+
+  constructor() {
+    super();
+    
+    const firebaseConfig = {
+      apiKey: "AIzaSyD-R7C9Sv4ybj5j8-F9MWIW3jrJTuAti6Y",
+      authDomain: "shopping-list-wojtek.firebaseapp.com",
+      projectId: "shopping-list-wojtek",
+      storageBucket: "shopping-list-wojtek.appspot.com",
+      messagingSenderId: "623332419240",
+      appId: "1:623332419240:web:a0be9d544363c338ae449d"
+    };
+
+    if (!firebase.apps.length){
+      firebase.initializeApp(firebaseConfig);
+      }
+
+      this.referenceShoppingList = firebase.firestore().collection('shoppinglist');
+
+      this.state = {
+        lists: []
+      };
+  }
+
+  componentDidMount() {
+    this.referenceShoppingList = firebase.firestore().collection('shoppinglist');
+    this.unsubscribe = this.referenceShoppingList.onSnapshot(this.onCollectionUpdate)
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const lists = [];
+    querySnapshot.forEach((doc) => {
+      var data = doc.data();
+      lists.push({
+        name: data.name,
+        items: data.items.toString(),
+      });
+    });
+    this.setState({
+      lists,
+    });
+  }
+
+  render() {
+    console.log('state', this.state.lists);
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>All Lists</Text>
+        <FlatList
+        data={this.state.lists}
+        renderItem={({ item }) => 
+        <Text style={styles.list}>{item.name}: {item.items}</Text>}
+        ></FlatList>
+        <StatusBar style="auto" />
+      </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +78,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 25
+  },
+  list: {
+    color: 'blue',
+    fontSize: 20
+  }
 });
